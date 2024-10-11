4c17b3f814b05c7f05cc6a6a7b96825cb801de21
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-10-11 11:27:31 +0530

    Added Clerk Authentication

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,18 +1,25 @@
-import { Editor } from '@/components/editor/Editor'
-import Header from '@/components/Header'
-import React from 'react'
+import { Editor } from "@/components/editor/Editor";
+import Header from "@/components/Header";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import React from "react";
 
 const Document = () => {
   return (
     <div>
-        <Header>
-            <div className='flex w-fit items-center justify-center gap-2'>
-                <p className='document-title'>This is a fake document</p>
-            </div>
-        </Header>
-        <Editor/>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Share</p>
+        </div>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </Header>
+      <Editor />
     </div>
-  )
-}
+  );
+};
 
-export default Document
+export default Document;
